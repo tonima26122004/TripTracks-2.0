c49614e7da919bd6f21dd4595a64804bbf394d9f
Atmajo <atmajoc@gmail.com>
2024-02-03 22:05:17 +0530

    Add routes for tomorrow and 7-day weather forecasts in WeatherHub component

--- src/Components/WeatherHub.jsx
+++ src/Components/WeatherHub.jsx
@@ -1,31 +1,41 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import WeatherHub_Nav from "./WeatherHub_Nav";
 import "../Style/WeatherHub.css";
 import { FiSun } from "react-icons/fi";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 function WeatherHub() {
+  const [activeTab, setActiveTab] = useState("tab1");
   const { place } = useParams();
+  const [data, setData] = useState({});
   
+  const styles = {
+    active: {
+      color: "#402E32",
+    },
+    inactive: {
+      opacity: "0.4",
+    },
+  };
+
   const apikey = "a0e9da674faffcda28aede6a39250e7e";
-  const url = `http://api.openweathermap.org/geo/1.0/direct?q=${place}&limit=1&appid=${apikey}`;
+  const url = `http://api.openweathermap.org/geo/1.0/direct?q=${place}&limit=2&appid=${apikey}`;
   let lon, lat;
   
   useEffect(() => {
     const fetchData = async () => {
       try {
         await axios.get(url).then((response) => {
-          lat = response.data[0].lat;
-          lon = response.data[0].lon;
+          lat = response.data[1].lat;
+          lon = response.data[1].lon;
+          console.log(lat, lon)
         });
         await axios
           .get(
             `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${apikey}`
           )
-          .then((response) => {
-            console.log(response.data);
-          });
+          .then((response) => {setData(response.data.current)});
       } catch (error) {
         console.log(error);
       }
@@ -66,33 +76,42 @@ function WeatherHub() {
           <div className="WeatherHub_right_Location">
             <p className="WeatherHub_right_Location-Name">{place}</p>
             <div className="WeatherHub_right_Location-codinate">
-              <a className="WeatherHub_right_Location-codinate1">4,000m</a>
-              <a className="WeatherHub_right_Location-codinate1">(3.28ft)</a>
+              <a className="WeatherHub_right_Location-codinate1">{data.pressure}pa</a>
             </div>
           </div>
           <div className="WeatherHub_temp">
             <FiSun className="WeatherHub_temp_icon" />
-            <a className="WeatherHub_Temp_lock">23°</a>
+            <a className="WeatherHub_Temp_lock">{Math.round(data.temp)}°</a>
             <a className="WeatherHub_temp-C">C</a>
           </div>
           <div className=""></div>
         </div>
         <div className="WeatherHub_left">
-          <img
-            className="Progress_Bar"
-            src="/images/Progress Bar.png"
-            alt="Progress Bar"
-          />
           <div className="WeatherHub_left_texts">
-            <a className="WeatherHub_left_text1" href="#">
+            <Link
+              className="WeatherHub_left_text1"
+              to={`/weatherHub/${place}`}
+              style={activeTab === "tab1" ? styles.active : styles.inactive}
+              onClick={() => setActiveTab("tab1")}
+            >
               Today
-            </a>
-            <a className="WeatherHub_left_text2" href="#">
+            </Link>
+            <Link
+              className="WeatherHub_left_text2"
+              to={`/weatherHub/${place}/tomorrow`}
+              style={activeTab === "tab2" ? styles.active : styles.inactive}
+              onClick={() => setActiveTab("tab2")}
+            >
               Tomorrow
-            </a>
-            <a className="WeatherHub_left_text3" href="#">
-              10 Days
-            </a>
+            </Link>
+            <Link
+              className="WeatherHub_left_text3"
+              to={`/weatherHub/${place}/7days`}
+              style={activeTab === "tab3" ? styles.active : styles.inactive}
+              onClick={() => setActiveTab("tab3")}
+            >
+              7 Days
+            </Link>
           </div>
         </div>
       </div>
